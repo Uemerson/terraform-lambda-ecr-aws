dd52af8d3f9902e1b1900c943a81c437551b93b0
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2025-11-30 02:58:13 -0300

    fix: github actions

--- infra/main.tf
+++ infra/main.tf
@@ -4,6 +4,11 @@ resource "aws_ecr_repository" "ecr_repository" {
   force_delete          = true
 }
 
+data "aws_ecr_image" "lambda_image" {
+  repository_name = aws_ecr_repository.ecr_repository.name
+  image_tag       = "latest"
+}
+
 resource "aws_iam_role" "lambda_role" {
   name = "${var.lambda_name}-exec-role"
 
@@ -29,7 +34,7 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_exec" {
 resource "aws_lambda_function" "lambda" {
   function_name = var.lambda_name
   package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.ecr_repository.repository_url}:latest"
+  image_uri     = "${aws_ecr_repository.ecr_repository.repository_url}@${data.aws_ecr_image.lambda_image.image_digest}"
   role          = aws_iam_role.lambda_role.arn
   timeout       = var.lambda_timeout
   memory_size   = var.lambda_memory
